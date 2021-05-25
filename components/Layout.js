10519a2238055932dba02d8eab7912b33e37654e
sattosan <sato.yuya1211@gmail.com>
2021-05-25 23:24:13 +0900

    changed to function

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 
-const Layout = ({ children, title = "Hp by Nextjs" }) => {
+export default function Layout({ children, title = "Hp by Nextjs" }) {
 	return (
 		<div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono">
 			<Head>
@@ -46,6 +46,4 @@ const Layout = ({ children, title = "Hp by Nextjs" }) => {
 			</footer>
 		</div>
 	);
-};
-
-export default Layout;
+}
